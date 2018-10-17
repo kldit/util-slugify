9433cd40811092767e4e3a3668bdc9e9bd0fcd91
Orlando Leite <orlleite@gmail.com>
2018-10-17 12:31:39 -0400

    Second commit

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ require('@kldit/util-clean-special-chars');
 
 Object.defineProperty(String.prototype, 'slugify',
 {
-	enumerable: false,
-	value: function ()
-	{
-		return this
-			.cleanSpecialChars()
-			.trim()
-			.toLowerCase()
-			.replace(/[^a-z0-9-]/, '-')
-			.replace(/-+/, '-');
-	}
+    enumerable: false,
+    value: function ()
+    {
+        return this
+            .cleanSpecialChars()
+            .trim()
+            .toLowerCase()
+            .replace(/[^a-z0-9-]/, '-')
+            .replace(/-+/, '-');
+    }
 });
